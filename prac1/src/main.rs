0790fbdfc4453b7bc4752fd98bc911e44e9d5847
Harsh <harshchalwadi027@gmail.com>
2024-09-16 01:06:59 +0530

    Added in folder prac1

---
+++ prac1/src/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let ans = is_even(20);
+    println!("{}" ,ans) 
+}
+
+fn is_even(num : i32)-> bool{
+    if num % 2 == 0 {
+        return  true;
+    }else {
+        return  false;
+    }
+}
---
+++ prac1/target/debug/deps/rust-143d1a9bd541b2f2.d
@@ -0,0 +1,5 @@
+D:\rust\target\debug\deps\librust-143d1a9bd541b2f2.rmeta: src/main.rs
+
+D:\rust\target\debug\deps\rust-143d1a9bd541b2f2.d: src/main.rs
+
+src/main.rs:
